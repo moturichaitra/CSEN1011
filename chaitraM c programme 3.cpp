fa8a5121c1da2bc42761cfa900970ec973c34c5e
moturichaitra <93367504+moturichaitra@users.noreply.github.com>
2021-11-26 12:31:26 +0530

    Add files via upload

---
+++ chaitraM c programme 3.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int basic,hra,da,pf,it,net;
+	printf("Enter the basics--\n");
+	scanf("%d",&basic);
+	printf("Enter the hra--\n");
+	scanf("%d",&hra);
+	printf("Enter the da--\n");
+	scanf("%d",&da);
+	printf("Enter the pf--\n");
+	scanf("%d",&pf);
+	printf("Enter the it--\n");
+	scanf("%d",&it);
+	printf ("calculated net salary is--%d\n",(basic+da+hra)-(pf+it));
+}
+	
+	
+	
+
+	
